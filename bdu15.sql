1762c5cf4d0f371af111519046c4adb80d7534d7
danhtuan-bdu <132867839+danhtuan-bdu@users.noreply.github.com>
2025-02-07 14:56:52 +0700

    Add files via upload

---
+++ bdu15.sql
@@ -0,0 +1,5 @@
+TRUNCATE TABLE django_celery_beat_periodictask;
+TRUNCATE TABLE django_celery_beat_periodictasks;
+SET FOREIGN_KEY_CHECKS = 0;
+TRUNCATE TABLE django_celery_beat_crontabschedule;
+SET FOREIGN_KEY_CHECKS = 1;
