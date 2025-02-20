ee43cd7ff764d74e2f5e4b6e628f5de68047e99c
danhtuan-bdu <132867839+danhtuan-bdu@users.noreply.github.com>
2025-02-20 21:18:54 +0700

    Add files via upload

---
+++ bdu26.sql
@@ -0,0 +1,19 @@
+use bdu_qlctdtn;
+
+SET NAMES 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10361 WHERE `ma_minh_chung_con` = 7434 AND `ma_minh_chung` = '[1]H9.09.02.08';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10362 WHERE `ma_minh_chung_con` = 7435 AND `ma_minh_chung` = '[1]H9.09.02.08';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10363 WHERE `ma_minh_chung_con` = 7436 AND `ma_minh_chung` = '[1]H9.09.02.08';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10364 WHERE `ma_minh_chung_con` = 7437 AND `ma_minh_chung` = '[1]H9.09.02.08';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10365 WHERE `ma_minh_chung_con` = 7438 AND `ma_minh_chung` = '[1]H9.09.02.08';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10366 WHERE `ma_minh_chung_con` = 7439 AND `ma_minh_chung` = '[1]H9.09.02.08';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10367 WHERE `ma_minh_chung_con` = 7440 AND `ma_minh_chung` = '[1]H9.09.02.08';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10368 WHERE `ma_minh_chung_con` = 7441 AND `ma_minh_chung` = '[1]H9.09.02.08';
