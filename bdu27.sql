504758df9a68f343dd1c39ff6c0ed2c537546801
danhtuan-bdu <132867839+danhtuan-bdu@users.noreply.github.com>
2025-02-21 06:51:04 +0700

    Add files via upload

---
+++ bdu27.sql
@@ -0,0 +1,95 @@
+use bdu_qlctdtn;
+
+SET NAMES 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10369 WHERE `ma_minh_chung_con` = 978 AND `ma_minh_chung` = '[2]H1.01.01.01';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10370 WHERE `ma_minh_chung_con` = 979 AND `ma_minh_chung` = '[2]H1.01.01.01';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10371 WHERE `ma_minh_chung_con` = 980 AND `ma_minh_chung` = '[2]H1.01.01.01';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10372 WHERE `ma_minh_chung_con` = 981 AND `ma_minh_chung` = '[2]H1.01.01.01';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10373 WHERE `ma_minh_chung_con` = 982 AND `ma_minh_chung` = '[2]H1.01.01.02';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10374 WHERE `ma_minh_chung_con` = 983 AND `ma_minh_chung` = '[2]H1.01.01.02';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10375 WHERE `ma_minh_chung_con` = 984 AND `ma_minh_chung` = '[2]H1.01.01.02';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10376 WHERE `ma_minh_chung_con` = 985 AND `ma_minh_chung` = '[2]H1.01.01.03';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10377 WHERE `ma_minh_chung_con` = 986 AND `ma_minh_chung` = '[2]H1.01.01.03';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10378 WHERE `ma_minh_chung_con` = 987 AND `ma_minh_chung` = '[2]H1.01.01.03';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10379 WHERE `ma_minh_chung_con` = 988 AND `ma_minh_chung` = '[2]H1.01.01.04';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10380 WHERE `ma_minh_chung_con` = 989 AND `ma_minh_chung` = '[2]H1.01.01.04';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10381 WHERE `ma_minh_chung_con` = 990 AND `ma_minh_chung` = '[2]H1.01.01.04';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10382 WHERE `ma_minh_chung_con` = 991 AND `ma_minh_chung` = '[2]H1.01.01.05';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10383 WHERE `ma_minh_chung_con` = 992 AND `ma_minh_chung` = '[2]H1.01.01.05';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10384 WHERE `ma_minh_chung_con` = 993 AND `ma_minh_chung` = '[2]H1.01.01.05';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10385 WHERE `ma_minh_chung_con` = 994 AND `ma_minh_chung` = '[2]H1.01.01.05';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10386 WHERE `ma_minh_chung_con` = 995 AND `ma_minh_chung` = '[2]H1.01.01.05';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10387 WHERE `ma_minh_chung_con` = 996 AND `ma_minh_chung` = '[2]H1.01.01.06';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10388 WHERE `ma_minh_chung_con` = 997 AND `ma_minh_chung` = '[2]H1.01.01.07';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10389 WHERE `ma_minh_chung_con` = 998 AND `ma_minh_chung` = '[2]H1.01.01.08';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10390 WHERE `ma_minh_chung_con` = 999 AND `ma_minh_chung` = '[2]H1.01.01.08';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10391 WHERE `ma_minh_chung_con` = 1000 AND `ma_minh_chung` = '[2]H1.01.01.09';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10392 WHERE `ma_minh_chung_con` = 1001 AND `ma_minh_chung` = '[2]H1.01.01.09';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10393 WHERE `ma_minh_chung_con` = 1002 AND `ma_minh_chung` = '[2]H1.01.02.01';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10394 WHERE `ma_minh_chung_con` = 1003 AND `ma_minh_chung` = '[2]H1.01.02.02';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10395 WHERE `ma_minh_chung_con` = 1004 AND `ma_minh_chung` = '[2]H1.01.02.03';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10396 WHERE `ma_minh_chung_con` = 1005 AND `ma_minh_chung` = '[2]H1.01.02.03';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10397 WHERE `ma_minh_chung_con` = 1006 AND `ma_minh_chung` = '[2]H1.01.02.04';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10398 WHERE `ma_minh_chung_con` = 1007 AND `ma_minh_chung` = '[2]H1.01.02.05';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10399 WHERE `ma_minh_chung_con` = 1008 AND `ma_minh_chung` = '[2]H1.01.03.01';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10400 WHERE `ma_minh_chung_con` = 1009 AND `ma_minh_chung` = '[2]H1.01.03.01';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10401 WHERE `ma_minh_chung_con` = 1010 AND `ma_minh_chung` = '[2]H1.01.03.02';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10402 WHERE `ma_minh_chung_con` = 1011 AND `ma_minh_chung` = '[2]H1.01.03.03';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10403 WHERE `ma_minh_chung_con` = 1012 AND `ma_minh_chung` = '[2]H1.01.03.04';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10404 WHERE `ma_minh_chung_con` = 1013 AND `ma_minh_chung` = '[2]H1.01.03.05';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung_con` = 10405 WHERE `ma_minh_chung_con` = 1014 AND `ma_minh_chung` = '[2]H1.01.03.06';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/1ITi9OoSxQuLE2Jg6kyWUer3sjLT3yfyq' WHERE `ma_minh_chung` = '[2]H9.09.02.09';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/1Sbbvb_kODP-f5nxj2fRIlG-tvggG9WJh' WHERE `ma_minh_chung` = '[4]H10.10.06.01';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/1P0LlObjhFeYvlc6ggKdfDgb8foUYOMR3' WHERE `ma_minh_chung` = '[4]H10.10.06.02';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/1q-mSou01iVYG4RseS0mkndRqZ1S-BVU0' WHERE `ma_minh_chung` = '[4]H10.10.06.03';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/1YXPcma7JbkaOeD-t31qEt80MQo6XAGpN' WHERE `ma_minh_chung` = '[4]H10.10.06.04';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/1Ci01vPQqiKCVyLIf7n1I7yULyszZVCgr' WHERE `ma_minh_chung` = '[4]H10.10.06.05';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/1ddjnTotRKJfy3h3MnVbw2ovx_RNYcB4I' WHERE `ma_minh_chung` = '[4]H10.10.06.06';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/18lZoXaFFouhAF1Db3dqiFEG3AcVVNvF9' WHERE `ma_minh_chung` = '[4]H10.10.06.07';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/1-cdbQ7N8ddZ9gnxHu93cbo05J8vvIqU_' WHERE `ma_minh_chung` = '[4]H10.10.06.08';
