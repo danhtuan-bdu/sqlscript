af38da1f39c5fb6d14b48e484cbca13fa3d0066a
danhtuan-bdu <132867839+danhtuan-bdu@users.noreply.github.com>
2025-02-21 17:07:09 +0700

    Add files via upload

---
+++ bdu34.sql
@@ -0,0 +1,13 @@
+use bdu_qlctdtn;
+
+SET NAMES 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `so_minh_chung` = '4' WHERE `ma_minh_chung_con` = 9500 AND `ma_minh_chung` = '[4]H1.01.01.05';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `so_minh_chung` = '5' WHERE `ma_minh_chung_con` = 9501 AND `ma_minh_chung` = '[4]H1.01.01.05';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `so_minh_chung` = '6' WHERE `ma_minh_chung_con` = 9502 AND `ma_minh_chung` = '[4]H1.01.01.05';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `so_minh_chung` = '7' WHERE `ma_minh_chung_con` = 9503 AND `ma_minh_chung` = '[4]H1.01.01.05';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `so_minh_chung` = '8' WHERE `ma_minh_chung_con` = 9504 AND `ma_minh_chung` = '[4]H1.01.01.05';
