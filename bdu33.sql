adeb790d9d26e3d888ca2c7db79f856235681ac0
danhtuan-bdu <132867839+danhtuan-bdu@users.noreply.github.com>
2025-02-21 16:34:52 +0700

    Add files via upload

---
+++ bdu33.sql
@@ -0,0 +1,39 @@
+use bdu_qlctdtn;
+
+SET NAMES 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `link` = '', `ngay_ban_hanh` = 'Số 2774/QĐ-ĐHBD, ngày 05/11/2019, Số 344/PHĐHBDCM, ngày 30/8/2023, Số 1135/QĐ-ĐHBD, ngày 21/6/2024' WHERE `ma_minh_chung_con` = 4595 AND `ma_minh_chung` = '[2]H7.07.04.03';
+
+DELETE FROM `bdu_qlctdtn`.`minh_chung_con` WHERE (`ma_minh_chung_con` = 4597 AND `ma_minh_chung` = '[2]H7.07.04.03') OR (`ma_minh_chung_con` = 4596 AND `ma_minh_chung` = '[2]H7.07.04.03');
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ngay_ban_hanh` = 'Số 188/BB-P.PTTT&TVTS, ngày 25/12/2017' WHERE `ma_minh_chung_con` = 6991 AND `ma_minh_chung` = '[3]H8.08.02.06';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ngay_ban_hanh` = 'Số 199/BB-P.PTTT&TVTS, ngày 30/12/2018' WHERE `ma_minh_chung_con` = 6992 AND `ma_minh_chung` = '[3]H8.08.02.06';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ngay_ban_hanh` = 'Số 193/BB-P.PTTT&TVTS, ngày 28/12/2020' WHERE `ma_minh_chung_con` = 6994 AND `ma_minh_chung` = '[3]H8.08.02.06';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ngay_ban_hanh` = 'Số 188/BB-P. PTTT&TVTS, ngày 25/12/2017' WHERE `ma_minh_chung_con` = 9974 AND `ma_minh_chung` = '[4]H8.08.02.06';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ngay_ban_hanh` = 'Số 199/BB-P. PTTT&TVTS, ngày 30/12/2018' WHERE `ma_minh_chung_con` = 9975 AND `ma_minh_chung` = '[4]H8.08.02.06';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ngay_ban_hanh` = 'Số 193/BB-P.PTTT&TVTS, ngày 28/12/2020' WHERE `ma_minh_chung_con` = 9977 AND `ma_minh_chung` = '[4]H8.08.02.06';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ngay_ban_hanh` = 'Số 188/BB-P.PTTT&TVTS, ngày 25/12/2017' WHERE `ma_minh_chung_con` = 6029 AND `ma_minh_chung` = '[5]H8.08.02.06';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ngay_ban_hanh` = 'Số 199/BB-P.PTTT&TVTS, ngày 30/12/2018' WHERE `ma_minh_chung_con` = 6030 AND `ma_minh_chung` = '[5]H8.08.02.06';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ngay_ban_hanh` = 'Số 193/BB-P.PTTT&TVTS, ngày 28/12/2020' WHERE `ma_minh_chung_con` = 6032 AND `ma_minh_chung` = '[5]H8.08.02.06';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ngay_ban_hanh` = 'Số 188/BB-P. PTTT&TVTS, ngày 25/12/2017' WHERE `ma_minh_chung_con` = 595 AND `ma_minh_chung` = '[1]H8.08.02.06';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ngay_ban_hanh` = 'Số 199/BB-P. PTTT&TVTS, ngày 30/12/2018' WHERE `ma_minh_chung_con` = 596 AND `ma_minh_chung` = '[1]H8.08.02.06';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ngay_ban_hanh` = 'Số 193/BB-P. PTTT&TVTS, ngày 28/12/2020' WHERE `ma_minh_chung_con` = 598 AND `ma_minh_chung` = '[1]H8.08.02.06';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ngay_ban_hanh` = 'Số 2774/QĐ-ĐHBD, ngày 05/11/2019, Số 344/PHĐHBDCM, ngày 30/8/2023, Số 1135/QĐ-ĐHBD, ngày 21/6/2024' WHERE `ma_minh_chung_con` = 9923 AND `ma_minh_chung` = '[4]H7.07.04.03';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ngay_ban_hanh` = 'Số 2774/QĐ-ĐHBD, ngày 05/11/2019, Số 344/PHĐHBDCM, ngày 30/8/2023, Số 1135/QĐ-ĐHBD, ngày 21/6/2024' WHERE `ma_minh_chung_con` = 5973 AND `ma_minh_chung` = '[5]H7.07.04.03';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ngay_ban_hanh` = 'Số 2774/QĐ-ĐHBD, ngày 05/11/2019, Số 344/PHĐHBDCM, ngày 30/8/2023, Số 1135/QĐ-ĐHBD, ngày 21/6/2024' WHERE `ma_minh_chung_con` = 7460 AND `ma_minh_chung` = '[1]H7.07.04.03';
+
+DELETE FROM `bdu_qlctdtn`.`minh_chung_con` WHERE (`ma_minh_chung_con` = 9924 AND `ma_minh_chung` = '[4]H7.07.04.03') OR (`ma_minh_chung_con` = 9925 AND `ma_minh_chung` = '[4]H7.07.04.03') OR (`ma_minh_chung_con` = 5974 AND `ma_minh_chung` = '[5]H7.07.04.03') OR (`ma_minh_chung_con` = 5975 AND `ma_minh_chung` = '[5]H7.07.04.03');
