1fa57a55b1e31254098284b9d2d226fab9f91210
danhtuan-bdu <132867839+danhtuan-bdu@users.noreply.github.com>
2025-02-21 11:32:07 +0700

    Add files via upload

---
+++ bdu28.sql
@@ -0,0 +1,76 @@
+use bdu_qlctdtn;
+
+SET NAMES 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci';
+
+INSERT INTO `minh_chung_con` (`ma_minh_chung_con`, `ma_minh_chung`, `ten_minh_chung`, `so_minh_chung`, `noi_ban_hanh`, `link`, `ngay_ban_hanh`) VALUES
+(10406, '[2]H11.11.05.01', 'Kế hoạch khảo sát sinh viên năm cuối', '1', 'P. QLĐT&CTSV', '', 'Số 261/KH -PĐT, ngày 04/10/2019, Số 120/KH-PĐT, ngày 25/05/2020,Số 176/ KH-QLĐT&CTSV, ngày 11/06/2021, Số 24/KH-QLĐT&CTSV, ngày 09/03/2022, Số 971/QĐ-ĐHBD, ngày 09/06/2023');
+
+INSERT INTO `minh_chung_con` (`ma_minh_chung_con`, `ma_minh_chung`, `ten_minh_chung`, `so_minh_chung`, `noi_ban_hanh`, `link`, `ngay_ban_hanh`) VALUES
+(10407, '[2]H11.11.05.01', 'Bảng câu hỏi khảo sát sinh viên về hoạt động giảng dạy', '2', 'P. QLĐT&CTSV', '', 'Số 1647/QĐ-ĐHBD, ngày 27/12/2022\nSố 812/QĐ-ĐHBD, ngày 07/05/2024\nSố 1653/QĐ-ĐHBD, ngày 09/08/2024');
+
+INSERT INTO `minh_chung_con` (`ma_minh_chung_con`, `ma_minh_chung`, `ten_minh_chung`, `so_minh_chung`, `noi_ban_hanh`, `link`, `ngay_ban_hanh`) VALUES
+(10408, '[2]H11.11.05.01', 'Bảng câu hỏi khảo sát sinh viên năm cuối', '3', 'P. QLĐT&CTSV', '', 'Số 1647/QĐ-ĐHBD, ngày 27/12/2022\nSố 812/QĐ-ĐHBD, ngày 07/05/2024\nSố 1653/QĐ-ĐHBD, ngày 09/08/2024');
+
+INSERT INTO `minh_chung_con` (`ma_minh_chung_con`, `ma_minh_chung`, `ten_minh_chung`, `so_minh_chung`, `noi_ban_hanh`, `link`, `ngay_ban_hanh`) VALUES
+(10409, '[2]H11.11.05.01', 'Bảng câu hỏi khảo sát cựu sinh viên', '4', 'P. QLĐT&CTSV', '', 'Số 1647/QĐ-ĐHBD, ngày 27/12/2022\nSố 812/QĐ-ĐHBD, ngày 07/05/2024\nSố 1653/QĐ-ĐHBD, ngày 09/08/2024');
+
+INSERT INTO `minh_chung_con` (`ma_minh_chung_con`, `ma_minh_chung`, `ten_minh_chung`, `so_minh_chung`, `noi_ban_hanh`, `link`, `ngay_ban_hanh`) VALUES
+(10410, '[2]H11.11.05.01', 'Kế hoạch khảo sát NH hoạt động giảng dạy (Khung kế hoạch đào tạo năm học)', '5', 'P. QLĐT&CTSV', '', '2019-2024');
+
+INSERT INTO `minh_chung_con` (`ma_minh_chung_con`, `ma_minh_chung`, `ten_minh_chung`, `so_minh_chung`, `noi_ban_hanh`, `link`, `ngay_ban_hanh`) VALUES
+(10411, '[2]H11.11.05.02', 'Báo cáo khảo sát đánh giá hoạt động giảng dạy', '', 'P. QLĐT&CTSV', '', '2021-2024');
+
+INSERT INTO `minh_chung_con` (`ma_minh_chung_con`, `ma_minh_chung`, `ten_minh_chung`, `so_minh_chung`, `noi_ban_hanh`, `link`, `ngay_ban_hanh`) VALUES
+(10412, '[2]H11.11.05.03', 'Báo cáo tổng kết năm học của Khoa', '', 'Khoa', '', '2019-2024');
+
+INSERT INTO `minh_chung_con` (`ma_minh_chung_con`, `ma_minh_chung`, `ten_minh_chung`, `so_minh_chung`, `noi_ban_hanh`, `link`, `ngay_ban_hanh`) VALUES
+(10413, '[2]H11.11.05.04', 'Báo cáo tổng kết năm học của Phòng QLĐT&CTSV', '', 'P. QLĐT&CTSV', '', 'Số 232/BC-QLĐT&CTSV, ngày 11/09/2020\nSố 211/BC-QLĐT&CTSV, ngày 06/09/2021\nSố 197/BC-QLĐT&CTSV, ngày 07/09/2022\nSố 971/QĐ-ĐHBD, ngày 09/06/2023\nSố 780/BC-QLĐT&CTSV, ngày 07/04/2024');
+
+INSERT INTO `minh_chung_con` (`ma_minh_chung_con`, `ma_minh_chung`, `ten_minh_chung`, `so_minh_chung`, `noi_ban_hanh`, `link`, `ngay_ban_hanh`) VALUES
+(10414, '[2]H11.11.05.05', 'Báo cáo tổng kết năm học của Trường', '', 'Trường ĐHBD', '', 'Số 1827/BC-ĐHBD, ngày 05/07/2019\nSố 644/BC-ĐHBD, ngày 19/11/2020\nSố 13/BC-ĐHBD, ngày 09/09/2021\nSố 134/BC-ĐHBD, ngày 08/03/2022\nSố 94/BC-ĐHBD, ngày 28/12/2023\nSố 64/BC-ĐHBD, ngày 10/09/2024');
+
+INSERT INTO `minh_chung_con` (`ma_minh_chung_con`, `ma_minh_chung`, `ten_minh_chung`, `so_minh_chung`, `noi_ban_hanh`, `link`, `ngay_ban_hanh`) VALUES
+(10415, '[2]H11.11.05.06', 'Báo cáo khảo sát các BLQ về CTĐT', '1', 'Khoa', '', '2019-2024');
+
+INSERT INTO `minh_chung_con` (`ma_minh_chung_con`, `ma_minh_chung`, `ten_minh_chung`, `so_minh_chung`, `noi_ban_hanh`, `link`, `ngay_ban_hanh`) VALUES
+(10416, '[2]H11.11.05.06', 'Báo cáo khảo sát sinh viên năm cuối', '2', 'P. QLĐT&CTSV', '', 'Số 397/BC-QLĐT&CTSV, ngày 31/12/2020\nSố 317/BC-QLĐT&CTSV, ngày 31/12/2021\nSố 243/BC-QLĐT&CTSV, ngày 12/10/2022\nSố 44/BC- ĐHBD, ngày 27/06/2024');
+
+INSERT INTO `minh_chung_con` (`ma_minh_chung_con`, `ma_minh_chung`, `ten_minh_chung`, `so_minh_chung`, `noi_ban_hanh`, `link`, `ngay_ban_hanh`) VALUES
+(10417, '[2]H10.10.03.06', 'Danh sách SV cảnh báo học vụ năm 2024', '5', 'Trường ĐHBD', '', 'Số 363/QĐ-ĐHBD, ngày 06/03/2024, Số 600/QĐ-ĐHBD, ngày 09/04/2024, Số 1972/QĐ-ĐHBD, ngày 17/09/2024');
+
+INSERT INTO `minh_chung_con` (`ma_minh_chung_con`, `ma_minh_chung`, `ten_minh_chung`, `so_minh_chung`, `noi_ban_hanh`, `link`, `ngay_ban_hanh`) VALUES
+(10418, '[3]H10.10.03.06', 'Danh sách SV cảnh báo học vụ năm 2024', '5', 'Trường ĐHBD', '', 'Số 363/QĐ-ĐHBD, ngày 06/03/2024, Số 600/QĐ-ĐHBD, ngày 09/04/2024, Số 1972/QĐ-ĐHBD, ngày 17/09/2024');
+
+INSERT INTO `minh_chung_con` (`ma_minh_chung_con`, `ma_minh_chung`, `ten_minh_chung`, `so_minh_chung`, `noi_ban_hanh`, `link`, `ngay_ban_hanh`) VALUES
+(10419, '[5]H10.10.03.06', 'Danh sách SV cảnh báo học vụ năm 2024', '5', 'Trường ĐHBD', '', 'Số 363/QĐ-ĐHBD, ngày 06/03/2024, Số 600/QĐ-ĐHBD, ngày 09/04/2024, Số 1972/QĐ-ĐHBD, ngày 17/09/2024');
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `so_thu_tu` = 7 WHERE `ma_minh_chung` = '[2]H10.10.03.07';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `so_thu_tu` = 8 WHERE `ma_minh_chung` = '[2]H10.10.03.08';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `so_thu_tu` = 9 WHERE `ma_minh_chung` = '[2]H10.10.03.09';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `so_thu_tu` = 10 WHERE `ma_minh_chung` = '[2]H10.10.03.10';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `so_thu_tu` = 10 WHERE `ma_minh_chung` = '[3]H10.10.03.10';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `so_thu_tu` = 9 WHERE `ma_minh_chung` = '[3]H10.10.03.09';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `so_thu_tu` = 8 WHERE `ma_minh_chung` = '[3]H10.10.03.08';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `so_thu_tu` = 7 WHERE `ma_minh_chung` = '[3]H10.10.03.07';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/12LGl4md3-pCbTK3D53wN6C8neTM-nZeo' WHERE `ma_minh_chung` = '[1]H9.09.02.09';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/1gUTbZQlDHHZQkFC06lD6_Igo6YMfs-bo' WHERE `ma_minh_chung` = '[3]H9.09.02.09';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/1B80PmCKaWbV7fWpQC5htn0P7uVqbblPp' WHERE `ma_minh_chung` = '[4]H9.09.02.09';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/1M2LZXL2irmX5-bmvnG8K-ScUTI94kNC-' WHERE `ma_minh_chung` = '[5]H9.09.02.09';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ngay_ban_hanh` = '2020, 2022, 2024' WHERE `ma_minh_chung_con` = 248 AND `ma_minh_chung` = '[1]H4.04.03.01';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ngay_ban_hanh` = '2020, 2022, 2024' WHERE `ma_minh_chung_con` = 249 AND `ma_minh_chung` = '[1]H4.04.03.01';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ngay_ban_hanh` = '2020, 2022, 2024' WHERE `ma_minh_chung_con` = 250 AND `ma_minh_chung` = '[1]H4.04.03.01';
+
