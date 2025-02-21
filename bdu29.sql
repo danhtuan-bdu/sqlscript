87805fb2458760e3e0b9e89dff985780905e77ba
danhtuan-bdu <132867839+danhtuan-bdu@users.noreply.github.com>
2025-02-21 14:00:34 +0700

    Add files via upload

---
+++ bdu29.sql
@@ -0,0 +1,7 @@
+use bdu_qlctdtn;
+
+SET NAMES 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ten_minh_chung` = 'Thông báo đăng ký NCKH' WHERE `ma_minh_chung_con` = 6859 AND `ma_minh_chung` = '[3]H6.06.06.06';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ten_minh_chung` = 'Thông báo đăng ký nhiệm vụ KH&CN cấp Trường' WHERE `ma_minh_chung_con` = 6879 AND `ma_minh_chung` = '[3]H6.06.07.03';
