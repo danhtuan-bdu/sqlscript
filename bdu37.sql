2431e8534a8966e22f6f93b8e3ae5b0563817222
danhtuan-bdu <132867839+danhtuan-bdu@users.noreply.github.com>
2025-02-22 14:13:00 +0700

    Add files via upload

---
+++ bdu37.sql
@@ -0,0 +1,33 @@
+use bdu_qlctdtn;
+
+SET NAMES 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ngay_ban_hanh` = '2020, 2022, 2024' WHERE `ma_minh_chung_con` = 278 AND `ma_minh_chung` = '[1]H5.05.01.03';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ngay_ban_hanh` = '2020, 2022, 2024' WHERE `ma_minh_chung_con` = 279 AND `ma_minh_chung` = '[1]H5.05.01.03';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ngay_ban_hanh` = '2020, 2022, 2024' WHERE `ma_minh_chung_con` = 280 AND `ma_minh_chung` = '[1]H5.05.01.03';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ngay_ban_hanh` = '2024' WHERE `ma_minh_chung_con` = 732 AND `ma_minh_chung` = '[1]H9.09.01.02';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/1A7dlGtw5vTieYxuwvN2EFMVlBycbGCvn' WHERE `ma_minh_chung` = '[1]H9.09.05.01';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/17xAdssRv4Q8cBTa7nZBXfzEQxw7Z7yMX' WHERE `ma_minh_chung` = '[1]H9.09.05.02';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/1QVn__sH32lCUaokR99pWL7f9tF-D01hq' WHERE `ma_minh_chung` = '[1]H9.09.05.03';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/1SjV5CIWSb22f0d8UDjaftsvpf32wquMb' WHERE `ma_minh_chung` = '[1]H9.09.05.04';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/1cTtLffvbsNRUEhnhQ5arXYG13Z7Ysaiz' WHERE `ma_minh_chung` = '[1]H9.09.05.05';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/1jEHO1RG9cG57b44suK-qCLsAZtYEvIpw' WHERE `ma_minh_chung` = '[1]H9.09.05.06';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/1tP5ePFS3UCb9__ByajfbAh2HuQfoTny3' WHERE `ma_minh_chung` = '[1]H9.09.05.07';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/139XSP8aZAVEoGduKcdZkJoR2-R3c22rB' WHERE `ma_minh_chung` = '[1]H9.09.05.08';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/1BuNscuWlFf1EjR8Nzr0pqnPa0gg0bsxh' WHERE `ma_minh_chung` = '[1]H9.09.05.09';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/1YgH7v1zMZTzPfoJDucDk_NBxGwgcUQ37' WHERE `ma_minh_chung` = '[1]H9.09.05.10';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/1zCdZ61Hav7aIBOQGRhHed7iP_O2MdJHP' WHERE `ma_minh_chung` = '[1]H9.09.05.11';
