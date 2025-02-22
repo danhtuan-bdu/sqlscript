6ad308d89e31fee127368349f282551fa8b44bef
danhtuan-bdu <132867839+danhtuan-bdu@users.noreply.github.com>
2025-02-22 09:55:18 +0700

    Add files via upload

---
+++ bdu36.sql
@@ -0,0 +1,31 @@
+use bdu_qlctdtn;
+
+SET NAMES 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ten_minh_chung` = 'Hình ảnh giao diện website Khoa CNTT tại địa chỉ https://fit.bdu.edu.vn' WHERE `ma_minh_chung_con` = 823 AND `ma_minh_chung` = '[1]H9.09.04.03';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ten_minh_chung` = 'Hình ảnh giao diện website Khoa Kinh tế tại địa chỉ https://kinhte.bdu.edu.vn' WHERE `ma_minh_chung_con` = 7232 AND `ma_minh_chung` = '[3]H9.09.04.03';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ten_minh_chung` = 'Hình ảnh giao diện website Khoa Kinh tế tại địa chỉ https://kinhte.bdu.edu.vn' WHERE `ma_minh_chung_con` = 10134 AND `ma_minh_chung` = '[4]H9.09.04.03';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ten_minh_chung` = 'Hình ảnh giao diện website Khoa Luật học tại địa chỉ https://luat.bdu.edu.vn' WHERE `ma_minh_chung_con` = 1709 AND `ma_minh_chung` = '[2]H9.09.04.03';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ten_minh_chung` = 'Biên bản họp tổng kết kết quả tuyển sinh năm 2019 - 2023', `so_minh_chung` = '', `ngay_ban_hanh` = '2019 - 2023' WHERE `ma_minh_chung_con` = 9948 AND `ma_minh_chung` = '[4]H8.08.01.04';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ten_minh_chung` = 'Phân tích dự báo nhu cầu', `ngay_ban_hanh` = '2019 - 2024' WHERE `ma_minh_chung_con` = 9953 AND `ma_minh_chung` = '[4]H8.08.01.05';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `noi_ban_hanh` = 'Trường ĐHBD' WHERE `ma_minh_chung_con` = 9954 AND `ma_minh_chung` = '[4]H8.08.01.06';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ngay_ban_hanh` = '2022 - 2024' WHERE `ma_minh_chung_con` = 9955 AND `ma_minh_chung` = '[4]H8.08.01.06';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ngay_ban_hanh` = '2019 - 2023' WHERE `ma_minh_chung_con` = 9956 AND `ma_minh_chung` = '[4]H8.08.01.07';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `noi_ban_hanh` = 'Trường ĐHBD' WHERE `ma_minh_chung_con` = 9967 AND `ma_minh_chung` = '[4]H8.08.02.05';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ngay_ban_hanh` = '2022 - 2024' WHERE `ma_minh_chung_con` = 9968 AND `ma_minh_chung` = '[4]H8.08.02.05';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ten_minh_chung` = 'Báo cáo công tác tuyển sinh thạc sĩ QTKD ', `ngay_ban_hanh` = '2019 - 2023' WHERE `ma_minh_chung_con` = 9969 AND `ma_minh_chung` = '[4]H8.08.02.05';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `so_minh_chung` = '', `ngay_ban_hanh` = '2019 - 2023' WHERE `ma_minh_chung_con` = 9974 AND `ma_minh_chung` = '[4]H8.08.02.06';
+
+DELETE FROM `bdu_qlctdtn`.`minh_chung_con` WHERE (`ma_minh_chung_con` = 9952 AND `ma_minh_chung` = '[4]H8.08.01.04') OR (`ma_minh_chung_con` = 9951 AND `ma_minh_chung` = '[4]H8.08.01.04') OR (`ma_minh_chung_con` = 9950 AND `ma_minh_chung` = '[4]H8.08.01.04') OR (`ma_minh_chung_con` = 9949 AND `ma_minh_chung` = '[4]H8.08.01.04') OR (`ma_minh_chung_con` = 9957 AND `ma_minh_chung` = '[4]H8.08.01.07') OR (`ma_minh_chung_con` = 9958 AND `ma_minh_chung` = '[4]H8.08.01.07') OR (`ma_minh_chung_con` = 9959 AND `ma_minh_chung` = '[4]H8.08.01.07') OR (`ma_minh_chung_con` = 9960 AND `ma_minh_chung` = '[4]H8.08.01.07') OR (`ma_minh_chung_con` = 9961 AND `ma_minh_chung` = '[4]H8.08.01.07') OR (`ma_minh_chung_con` = 9962 AND `ma_minh_chung` = '[4]H8.08.01.07') OR (`ma_minh_chung_con` = 9970 AND `ma_minh_chung` = '[4]H8.08.02.05') OR (`ma_minh_chung_con` = 9971 AND `ma_minh_chung` = '[4]H8.08.02.05') OR (`ma_minh_chung_con` = 9972 AND `ma_minh_chung` = '[4]H8.08.02.05') OR (`ma_minh_chung_con` = 9973 AND `ma_minh_chung` = '[4]H8.08.02.05') OR (`ma_minh_chung_con` = 9975 AND `ma_minh_chung` = '[4]H8.08.02.06') OR (`ma_minh_chung_con` = 9976 AND `ma_minh_chung` = '[4]H8.08.02.06') OR (`ma_minh_chung_con` = 9977 AND `ma_minh_chung` = '[4]H8.08.02.06') OR (`ma_minh_chung_con` = 9978 AND `ma_minh_chung` = '[4]H8.08.02.06') OR (`ma_minh_chung_con` = 9979 AND `ma_minh_chung` = '[4]H8.08.02.06') OR (`ma_minh_chung_con` = 9980 AND `ma_minh_chung` = '[4]H8.08.02.06');
