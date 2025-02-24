3f67730ae4bcda5b25e83d888e56b1a11b23a7db
danhtuan-bdu <132867839+danhtuan-bdu@users.noreply.github.com>
2025-02-24 17:46:12 +0700

    Add files via upload

---
+++ bdu38.sql
@@ -0,0 +1,15 @@
+use bdu_qlctdtn;
+
+SET NAMES 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/1YhAo-O4a88PGV0xQ7ENylv458rUr1CT5' WHERE `ma_minh_chung` = '[4]H10.10.04.01';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/1GPMESR2bh53niHLrt_4p4WlNBxfGEVGU' WHERE `ma_minh_chung` = '[4]H10.10.04.02';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/1ZJWjJ1nbl3zGxXMGm15xFb-AQ-GpnHuB' WHERE `ma_minh_chung` = '[4]H10.10.04.03';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/16c39jAOLCoXBwnB1LJquejfLpDf9_2d_' WHERE `ma_minh_chung` = '[4]H10.10.04.05';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/13-cHKzunuS5Dm8UWNQTvE3AdFjgV4zES' WHERE `ma_minh_chung` = '[4]H10.10.04.04';
+
+UPDATE `bdu_qlctdtn`.`minh_chung` SET `url` = 'https://drive.google.com/drive/folders/1CtMM1wBTdxFvgNFLrKLceGY7nKopcGi7' WHERE `ma_minh_chung` = '[4]H10.10.04.06';
