ba7b6087464e273692111033a603027ec6e9f92b
danhtuan-bdu <132867839+danhtuan-bdu@users.noreply.github.com>
2025-03-10 16:16:33 +0700

    Add files via upload

---
+++ bdu39.sql
@@ -0,0 +1,11 @@
+use bdu_qlctdtn;
+
+UPDATE `bdu_qlctdtn`.`bc_tdg` SET `url` = 'https://drive.google.com/file/d/1xYIAuGhdADyxaT2en_C_mnBUCbPvJVfF/view' WHERE `ma_bc_tdg` = '1';
+
+UPDATE `bdu_qlctdtn`.`bc_tdg` SET `url` = 'https://drive.google.com/file/d/1wGwz0YXoXXVkqDok8XJjGmL6kWjMbKTH/view' WHERE `ma_bc_tdg` = '2';
+
+UPDATE `bdu_qlctdtn`.`bc_tdg` SET `url` = 'https://drive.google.com/file/d/1gAfvfDQj3YHasXgMdOhFgj0sB_kY4IS0/view' WHERE `ma_bc_tdg` = '3';
+
+UPDATE `bdu_qlctdtn`.`bc_tdg` SET `url` = 'https://drive.google.com/file/d/1cw58QleObyfZavAYYoGIEqZ19LQu_swc/view' WHERE `ma_bc_tdg` = '4';
+
+UPDATE `bdu_qlctdtn`.`bc_tdg` SET `url` = 'https://drive.google.com/file/d/1PHvQyEBWE5SAXfIEgZ9gjax_2GdRmEIb/view' WHERE `ma_bc_tdg` = '5';
