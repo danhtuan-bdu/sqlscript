1f620e65bab618c3b36f65b2b3b663b84e2d918f
danhtuan-bdu <132867839+danhtuan-bdu@users.noreply.github.com>
2025-03-12 20:49:57 +0700

    Add files via upload

---
+++ bdu41.sql
@@ -0,0 +1,5 @@
+use bdu_qlctdtn;
+
+SET NAMES 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci';
+
+UPDATE `bdu_qlctdtn`.`minh_chung_con` SET `ma_minh_chung` = '[5]H1.01.03.BS01' WHERE `ma_minh_chung_con` = 10630 AND `ma_minh_chung` = '[5]H1.01.03.BS02';
