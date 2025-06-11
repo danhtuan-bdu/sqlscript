ae5b955f754050764d74be923642b77343f74c2f
danhtuan-bdu <132867839+danhtuan-bdu@users.noreply.github.com>
2025-06-11 14:56:14 +0700

    Add files via upload

---
+++ bctm11062005.sql
@@ -0,0 +1,8 @@
+USE bctm_bdu;
+
+UPDATE donvi SET tenDonVi = 'Tổ Công nghệ thông tin' WHERE maDonVi = 4;
+UPDATE donvi SET tenDonVi = 'Trung tâm Chuyển đổi số' WHERE maDonVi = 5;
+UPDATE donvi SET tenDonVi = 'Trung tâm Steam' WHERE maDonVi = 6;
+UPDATE donvi SET tenDonVi = 'CLB Khoa công nghệ số FIRA' WHERE maDonVi = 9;
+
+UPDATE loainoidung SET tenLoaiNoiDung = 'Nội dung công việc chưa thực hiện trong tuần' WHERE maLoaiNoiDung = 2;
